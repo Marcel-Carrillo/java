3093477874803fc9c091493a32e94734778350ee
marcel <mcarhue.ti@gmail.com>
2023-06-16 14:11:30 +0200

    Ejercicio IMC

---
+++ basicos/src/basicosprofe/IMC.java
@@ -0,0 +1,60 @@
+package basicosprofe;
+import java.util.Scanner;
+
+
+public class IMC {
+	public static void main(String[] args) {
+		
+			peticion();
+	}
+	
+	
+	private static void peticion(){
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingesa la estatura en metros: ");
+		float estatura = scanner.nextFloat();
+		System.out.println("Ingesa el peso en kilos: ");
+		float peso = scanner.nextFloat();
+		
+		float estaturaFinal = estatura * estatura;
+	
+		
+		float imcFinal = peso / estaturaFinal;
+		
+		if(imcFinal < 16) {
+			System.out.println("Desnutrido");
+		}else if(imcFinal >= 16 && imcFinal < 18) {
+			System.out.println("Delgado");
+		}else if(imcFinal >= 18 && imcFinal < 25){
+			System.out.println("Ideal");
+		}else if(imcFinal >= 25 && imcFinal <31) {
+			System.out.println("Sobrepeso");
+		}else if(imcFinal >= 31) {
+			System.out.println("Obeso");
+		}
+		
+		System.out.println("Este es tu indice de masa corporal "+imcFinal);
+		scanner.close();
+	}
+	
+	
+}
+
+
+/**
+ * TODO
+ * HACED UNA APLICACIÓN QUE PERMITA INTRODUCIR AL USUARIO
+ * SU ESTATURA EN METROS Y SU PESO EN KG Y LE INFORME SU 
+ * INFORME DE SU ÍNDICE DE MASA CORPORAL (IMC) SEGÚN LA SIGUIENTE FÓRMULA
+ * 
+ * LA FÓRMULA DEL IMC = PESO KG / ESTATURA AL CUADRADO METROS
+ *  * SI IMC < 16 ->> su imc es DESNUTRIDO
+ * SI IMC >= 16 Y < 18  ->> su imc es DELGADO
+ * SI IMC >= 18 Y < 25  ->> su imc es IDEAL
+ * SI IMC >= 25 Y < 31  ->> su imc es SOBREPESO
+ * SI IMC >= 31 ->> su imc es OBESO
+ ADEMÁS, DE DECIRLE SU IMC NUMÉRICO Y NOMINAL,
+pista: PARA LEER DE TECLADO USAD LA CLASE SCANNER
+  */
